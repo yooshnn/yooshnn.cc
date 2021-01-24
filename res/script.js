6444f487e772477d7675fb53f0b4465148d508b4
yooshnn <62302421+Yooshnn@users.noreply.github.com>
2021-01-24 14:14:53 +0900

    Update script.js

--- res/script.js
+++ res/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJSON("https://raw.githubusercontent.com/Yooshnn/yooshnn.cc/master/val.json", function(data) {
+    $.getJSON("https://raw.githubusercontent.com/Yooshnn/yooshnn.cc/main/res/val.json", function(data) {
         $("#bojSolve").text(" (" + data.bojSolve + " Solves)");
         $("#cfRating").text(" (" + data.cfRating + ", Expert)");
         console.log(data)
